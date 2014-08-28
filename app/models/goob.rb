746cdebf2314e7619ede7ce31c30636e1a05a038
John <john.carruth117@gmail.com>
2014-08-27 21:43:32 -0500

    debug

--- app/models/goob.rb
+++ app/models/goob.rb
@@ -1,7 +1,7 @@
 class Goob < ActiveRecord::Base
 	belongs_to :user
 	
-	has_attached_file :image, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" },
+	has_attached_file :image, :styles => { :large => "450x450", :medium => "300x300>", :thumb => "100x100>" },
 							  :storage => :s3,
 							  :bucket => 'Shmeebler'
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
