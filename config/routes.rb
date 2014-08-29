04e6ea405bd6624b3e35c285f29f4abcdeb6654b
John <john.carruth117@gmail.com>
2014-08-28 22:39:10 -0500

    Changed root path so that it's routed to index - showing goobs on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :goobs
 
   devise_for :users
-  root "pages#home"
+  root "goobs#index"
   get "about" => "pages#about"  # creates about_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
