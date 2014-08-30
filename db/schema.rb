4be2e59f25971098fed3e7ba54417c082cc89f15
John <john.carruth117@gmail.com>
2014-08-30 18:35:42 -0500

    working on first name and last name form fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826022008) do
+ActiveRecord::Schema.define(version: 20140830230524) do
 
   create_table "goobs", force: true do |t|
     t.string   "description"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 20140826022008) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
